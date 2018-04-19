f8bafd419809f5091a48d380f6d1dbf7bcbc8e56
dcorn068 <dcorn068@users.noreply.github.com>
2018-04-19 14:04:46 -0400

    Added a circle and moved it to the right.

---
+++ js/Visualization.js
@@ -0,0 +1,6 @@
+	d3.select("circle").transition().duration(1500)
+		.style("opacity","0.1")
+		.attr("r","100")
+		.attr("cx","200")
+
+d3.csv()
---
+++ serverDesktop.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Daniel2\Documents\dataviz1
+browser-sync start --server --port 8888 --files="**/*"
